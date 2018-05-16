56f393c74cbb53a29f536818b35fa37df57c3af1
Carolina Mesquita <carolti2013@gmail.com>
2018-05-15 23:16:51 -0300

    desenvolvimento front

--- controller/clienteController.js
+++ controller/clienteController.js
@@ -8,9 +8,11 @@ class ClienteController {
     constructor(app) {
 
         app.get('/clientes', this.findAll)
+        app.get('/clientes/:id', this.findOne)
         app.post('/clientes', this.create)
-        app.put('/clientes', this.update)
-        app.delete('/clientes', this.remove)
+        app.put('/clientes/:id', this.update)
+        app.delete('/clientes/:id', this.delete)
+        
         //this.model recebe o acesso a colletion no banco de dados
        model = mongoose.model('Cliente')
         
@@ -20,18 +22,23 @@ class ClienteController {
     async findAll (req,res) {
        res.json(await model.find());
     }
+    async findOne (req, res){
+        let id = req.params.id;
+        res.json(await model.findById(id));
+    }
     async create(req,res){
         let cliente = req.body;
         res.json(await model.create(cliente));
 
     }
     async update(req,res){
-        let cliente = req.body;
-        res.json(await model.update({_id: cliente._id, cliente}));
+        let _id = req.params.id;
+        res.json(await model.findOneAndUpdate({ _id }, req.body));
     }
     async delete(req, res){
-        let id = req.body._id;
-        res.json(await model.remove({_id:id}));
+        let _id = req.param._id;
+        res.json(await model.findOneAndRemove({ _id }, req.body));
     }
+    
 }
 module.exports = ClienteController;
---
+++ vue_crud/src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      fixed
+      
+      :clipped="clipped"
+      v-model="drawer"
+      app
+    >
+      <v-list>
+        <v-list-tile 
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed app :clipped-left="clipped">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+     
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+            
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+          <v-layout column align-center>
+           <v-carousel>
+    <v-carousel-item v-for="(item,i) in items" :src="item.src" :key="i"></v-carousel-item>
+  </v-carousel>
+          </v-layout>
+           
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+    <v-navigation-drawer
+      temporary
+      :right="right"
+      v-model="rightDrawer"
+      fixed
+    >
+      <v-list>
+        <v-list-tile @click.native="right = !right">
+          <v-list-tile-action>
+            <v-icon>compare_arrows</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+   <v-footer height="auto">
+    <v-card
+      flat
+      tile
+      class="indigo lighten-1 white--text text-xs-center"
+    >
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          icon
+          class="mx-3 white--text"
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+      <v-card-text class="white--text pt-0">
+        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </v-card-text>
+      <v-card-text class="white--text">
+        &copy;2018 — <strong>Vuetify</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [{ icon: "bubble_chart", title: "Home" }],
+    
+      title: "Fabrica de Programador"
+    };
+  }
+};
+</script>
